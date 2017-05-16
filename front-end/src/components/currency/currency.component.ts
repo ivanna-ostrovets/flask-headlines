7bbc58696ab492a4e39c134ac63d1d73a8c125cd
ivanna-ostrovets <ivanna.ostrovets@gmail.com>
2017-05-16 17:20:32 +0300

    Start refactor currency

--- front-end/src/components/currency/currency.component.ts
+++ front-end/src/components/currency/currency.component.ts
@@ -1,22 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewChecked } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 import { APP_SERVER } from '../../app/app.module';
 
+const $ = require('jquery');
+
 @Component({
   selector: 'hl-currency',
-  templateUrl: './currency.component.html'
+  templateUrl: './currency.component.html',
+  styleUrls: ['./currency.component.scss']
 })
-export class CurrencyComponent implements OnInit {
+export class CurrencyComponent implements OnInit, AfterViewChecked {
   currencies: string[];
   fromCurreny: string;
   rate: number;
   toCurrency: string;
 
-  constructor(
-    private http: Http
-  ) {
+  constructor(private http: Http) {
   }
 
   ngOnInit() {
@@ -26,8 +27,19 @@ export class CurrencyComponent implements OnInit {
       .subscribe(
         (data) => {
           this.currencies = data;
+          console.log(this.currencies);
         }
       );
+
+    $(document).ready(function () {
+      $('select').material_select();
+    });
+  }
+
+  ngAfterViewChecked() {
+    $(document).ready(function () {
+      $('select').material_select();
+    });
   }
 
   getRate() {
