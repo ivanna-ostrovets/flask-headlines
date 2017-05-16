4ad063f75cd753061ea58b415167d39919ad8cff
ivanna-ostrovets <ivanna.ostrovets@gmail.com>
2017-05-16 12:52:07 +0300

    Refactor

--- front-end/src/components/navbar/navbar.component.ts
+++ front-end/src/components/navbar/navbar.component.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+import { APP_SERVER } from '../../app/app.module';
 
 @Component({
   selector: 'hl-header',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  channels: string[];
+
+  constructor(
+    private http: Http
+  ) {
+  }
+
+  ngOnInit() {
+    this.http
+      .get(APP_SERVER + 'api/news')
+      .map((response: Response) => response.json())
+      .subscribe(
+        (data) => {
+          this.channels = data;
+        }
+      );
+  }
+
+  selectTab(tabId: string) {
+    document.getElementById(tabId).click();
+  }
 }
