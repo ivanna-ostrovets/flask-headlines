28adc7fafc7b7df66372c1f16dab656803312c14
ivanna-ostrovets <ivanna.ostrovets@gmail.com>
2017-05-16 13:24:31 +0300

    Refactor weather

--- front-end/src/components/weather/weather.component.ts
+++ front-end/src/components/weather/weather.component.ts
@@ -8,24 +8,31 @@ import { APP_SERVER } from '../../app/app.module';
 
 @Component({
   selector: 'hl-weather',
-  templateUrl: './weather.component.html'
+  templateUrl: './weather.component.html',
+  styleUrls: ['./weather.component.scss']
 })
 export class WeatherComponent {
   city: string;
+  errorMessage: string;
   weather: Weather;
 
-  constructor(
-    private http: Http
-  ) {
+  constructor(private http: Http) {
   }
 
   getData() {
+    this.weather = null;
+
     this.http
       .get(APP_SERVER + `api/weather?city=${this.city}`)
       .map((response: Response) => response.json())
       .subscribe(
         (data) => {
           this.weather = data;
+          this.errorMessage = null;
+        },
+
+        (error) => {
+          this.errorMessage = 'Invalid city';
         }
       );
   }
